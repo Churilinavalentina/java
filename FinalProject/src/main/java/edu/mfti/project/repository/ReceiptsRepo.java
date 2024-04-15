75924cc00ec4cbf3ff7f0dd40bb6119dd68245e7
Valentina Kozub <churilina_v@mail.ru>
2024-04-15 17:25:33 +0300

    fix

--- FinalProject/src/main/java/edu/mfti/project/repository/ReceiptsRepo.java
+++ FinalProject/src/main/java/edu/mfti/project/repository/ReceiptsRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ReceiptsRepo extends CrudRepository<Receipts, Integer> {
-    List<Book> findAllByBookId(String book_id);
+
 }
